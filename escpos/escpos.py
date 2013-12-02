42fe102396e499e4ddf86624d15c47023022ff0b
agb80 <atin81@gmail.com>
2013-12-02 09:52:54 -0600

    Added syntax to have print working on windows

--- escpos/escpos.py
+++ escpos/escpos.py
@@ -63,7 +63,7 @@ class Escpos:
 
 
         if im.size[0] > 512:
-            print  "WARNING: Image is wider than 512 and could be truncated at print time "
+            print  ("WARNING: Image is wider than 512 and could be truncated at print time ")
         if im.size[1] > 255:
             raise ImageSizeError()
 
@@ -181,37 +181,6 @@ class Escpos:
 
     def set(self, align='left', font='a', type='normal', width=1, height=1):
         """ Set text properties """
-        # Align
-        if align.upper() == "CENTER":
-            self._raw(TXT_ALIGN_CT)
-        elif align.upper() == "RIGHT":
-            self._raw(TXT_ALIGN_RT)
-        elif align.upper() == "LEFT":
-            self._raw(TXT_ALIGN_LT)
-        # Font
-        if font.upper() == "B":
-            self._raw(TXT_FONT_B)
-        else:  # DEFAULT FONT: A
-            self._raw(TXT_FONT_A)
-        # Type
-        if type.upper() == "B":
-            self._raw(TXT_BOLD_ON)
-            self._raw(TXT_UNDERL_OFF)
-        elif type.upper() == "U":
-            self._raw(TXT_BOLD_OFF)
-            self._raw(TXT_UNDERL_ON)
-        elif type.upper() == "U2":
-            self._raw(TXT_BOLD_OFF)
-            self._raw(TXT_UNDERL2_ON)
-        elif type.upper() == "BU":
-            self._raw(TXT_BOLD_ON)
-            self._raw(TXT_UNDERL_ON)
-        elif type.upper() == "BU2":
-            self._raw(TXT_BOLD_ON)
-            self._raw(TXT_UNDERL2_ON)
-        elif type.upper == "NORMAL":
-            self._raw(TXT_BOLD_OFF)
-            self._raw(TXT_UNDERL_OFF)
         # Width
         if width == 2 and height != 2:
             self._raw(TXT_NORMAL)
@@ -224,6 +193,38 @@ class Escpos:
             self._raw(TXT_2HEIGHT)
         else: # DEFAULT SIZE: NORMAL
             self._raw(TXT_NORMAL)
+        # Type
+        if type.upper() == "B":
+            self._raw(TXT_BOLD_ON)
+            self._raw(TXT_UNDERL_OFF)
+        elif type.upper() == "U":
+            self._raw(TXT_BOLD_OFF)
+            self._raw(TXT_UNDERL_ON)
+        elif type.upper() == "U2":
+            self._raw(TXT_BOLD_OFF)
+            self._raw(TXT_UNDERL2_ON)
+            self._raw(TXT_ITALIC_OFF)
+        elif type.upper() == "BU":
+            self._raw(TXT_BOLD_ON)
+            self._raw(TXT_UNDERL_ON)
+        elif type.upper() == "BU2":
+            self._raw(TXT_BOLD_ON)
+            self._raw(TXT_UNDERL2_ON)
+        elif type.upper == "NORMAL":
+            self._raw(TXT_BOLD_OFF)
+            self._raw(TXT_UNDERL_OFF)
+        # Font
+        if font.upper() == "B":
+            self._raw(TXT_FONT_B)
+        else:  # DEFAULT FONT: A
+            self._raw(TXT_FONT_A)
+        # Align
+        if align.upper() == "CENTER":
+            self._raw(TXT_ALIGN_CT)
+        elif align.upper() == "RIGHT":
+            self._raw(TXT_ALIGN_RT)
+        elif align.upper() == "LEFT":
+            self._raw(TXT_ALIGN_LT)
 
 
     def cut(self, mode=''):
